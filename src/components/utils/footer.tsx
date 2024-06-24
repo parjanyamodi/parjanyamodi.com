a698795f10155272fecdbaa68f8829a6fe000320
Parjanya Modi <parjanya.cs19@bmsce.ac.in>
2024-06-24 11:50:27 +0530

    FIX: footer cp year and spacing

--- src/components/utils/footer.tsx
+++ src/components/utils/footer.tsx
@@ -1,5 +1,5 @@
 export default function Footer() {
-  return <div className="flex flex-row w-full h-full justify-center items-center px-12 xl:px-64 lg:px-44 md:px-30 py-4">
-    <p className="text-gray-500">© 2021 Parjanya Modi. All rights reserved.</p>
+  return <div className="flex flex-row w-full h-full items-center px-8 xl:px-64 lg:px-44 md:px-30 py-4">
+    <p className="text-gray-500">© {(new Date()).getFullYear()} Parjanya Modi. All rights reserved.</p>
   </div>
 }
